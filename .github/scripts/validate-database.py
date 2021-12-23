9e8a6cfe23689a6d9067b201e4096cc405dec8ac
Speeder <ek.speeder@gmail.com>
2021-12-23 21:50:57 +0100

    Added file existence checking to the CI

---
+++ .github/scripts/validate-database.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import argparse
+from json import load
+from jsonschema import validate
+from os import path
+
+def walkJson(data, proc):
+    if isinstance(data, dict):
+        for k, v in data.items():
+            proc(k, v)
+            walkJson(v, proc)
+    elif isinstance(data, list):
+        for item in data:
+            walkJson(item, proc)
+
+class Checker:
+    def __init__(self, rootDir):
+        self.rootDir = rootDir
+
+    def __call__(self, key, value):
+        if key == "kml":
+            if not path.exists(path.join(self.rootDir, value)):
+                raise ValueError(value + " does not exist.")
+        if key == "md" and value.endswith(".md"):
+            if not path.exists(path.join(self.rootDir, value)):
+                raise ValueError(value + " does not exist.")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="The JSON file to validate")
+    parser.add_argument("schema", help="The JSON schema to use for validation")
+    args = parser.parse_args()
+
+    data = {}
+    with open(args.schema) as s:
+        schema = load(s)
+        with open(args.file) as f:
+            data = load(f)
+            validate(instance=data, schema=schema)
+
+    walkJson(data, Checker(path.dirname(args.file)))
+
+    print("Finished successfully.")
+
+if __name__ == "__main__":
+    main()
