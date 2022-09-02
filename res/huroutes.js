2b8271f77f1e4777e1716e3c1a85822d0b18831e
Speeder <ek.speeder@gmail.com>
2022-09-02 16:02:41 +0200

    Reset the map view fully when pressing back the last time

--- res/huroutes.js
+++ res/huroutes.js
@@ -438,6 +438,17 @@ function updateOptions()
     }
 }
 
+function removeFocus()
+{
+    map.eachLayer(layer => {
+        if (layer.focused)
+        {
+            layer.setStyle(layer.options.style);
+            layer.focused = false;
+        }
+    });
+}
+
 function activateRoute(layerOrRouteId)
 {
     var layer = null
@@ -453,13 +464,7 @@ function activateRoute(layerOrRouteId)
     else
         layer = layerOrRouteId;
 
-    map.eachLayer(layer => {
-        if (layer.focused)
-        {
-            layer.setStyle(layer.options.style);
-            layer.focused = false;
-        }
-    });
+    removeFocus()
     layer.setStyle(layer.options.focusedStyle);
     layer.focused = true;
 
@@ -491,7 +496,11 @@ window.addEventListener('popstate', event => {
     if (event.state)
         activateRoute(event.state)
     else
+    {
+        $('.collapse').collapse('hide')
+        removeFocus()
         map.flyToBounds(huroutes.opt.map.bounds);
+    }
 });
 
 function normRating(rat)
