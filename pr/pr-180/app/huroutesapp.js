44041e6ef09ef1096f199f0a4bfcad1c3a942db3
Sp3EdeR <Sp3EdeR@users.noreply.github.com>
2024-03-12 19:41:11 +0000

    Deploy preview for PR 180 🛫

--- pr/pr-180/app/huroutesapp.js
+++ pr/pr-180/app/huroutesapp.js
@@ -1,6 +1,5 @@
 (function(){function toast(event)
-{event.preventDefault();var toast=$('#toast-pwa-app');toast.toast('show');toast.on('hide.bs.toast',()=>localStorage.shownPwaAd=true);toast.find('a[href]').on('click',async()=>{const ret=await event.prompt();if(ret=="accepted")
-toast.toast('hide');});}
+{event.preventDefault();var toast=$('#toast-pwa-app');toast.toast('show');toast.on('hide.bs.toast',()=>localStorage.shownPwaAd=true);toast.find('a[href]').on('click',async()=>{event.prompt();toast.toast('hide');});}
 if(!localStorage.shownPwaAd)
 window.addEventListener("beforeinstallprompt",event=>setTimeout(()=>toast(event),5000));if('serviceWorker'in navigator)
 navigator.serviceWorker.register('service-worker.js');})();
