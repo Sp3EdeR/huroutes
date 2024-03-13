3c1dc5356df832c2a574163cd9f0dae4dd23c000
Sp3EdeR <Sp3EdeR@users.noreply.github.com>
2024-03-13 20:24:42 +0000

    Deploy preview for PR 180 🛫

--- pr/pr-180/app/huroutesapp.js
+++ pr/pr-180/app/huroutesapp.js
@@ -1,9 +1,10 @@
-(function(){function toast(event)
-{event.preventDefault();var toast=$('#toast-pwa-app');toast.toast('show');toast.on('hide.bs.toast',()=>localStorage.shownPwaAd=true);toast.find('a[href]').on('click',()=>{event.prompt();toast.toast('hide');return false;});}
-function iosToast()
-{}
+(function(){if(navigator.userAgent.match(/(Windows|Macintosh|X11).*(Opera|OPR)/))
+return;function toast(id)
+{var toast=$(id);toast.toast('show');toast.on('hide.bs.toast',()=>localStorage.shownPwaAd=true);return toast;}
+function pwaToast(event)
+{event.preventDefault();var toast=toast('#toast-pwa-app');toast.find('a[href]').on('click',()=>{event.prompt();toast.toast('hide');return false;});}
 if(!localStorage.shownPwaAd)
-{window.addEventListener("beforeinstallprompt",event=>setTimeout(()=>toast(event),5000));if(navigator.userAgent.match(/iP(ad|od|hone).*Safari/)&&!navigator.standalone&&!window.matchMedia('(display-mode: standalone)').matches)
-iosToast();}
+{window.addEventListener("beforeinstallprompt",event=>setTimeout(()=>pwaToast(event),5000));if(navigator.userAgent.match(/iP(ad|od|hone).*Safari/)&&!navigator.standalone&&!window.matchMedia('(display-mode: standalone)').matches)
+toast('#toast-safari-app');}
 if('serviceWorker'in navigator)
 navigator.serviceWorker.register('service-worker.js');})();
