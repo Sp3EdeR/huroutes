04a8299275e6ddc7319caad2a70330abe3c3c8ab
Sp3EdeR <ek.speeder@gmail.com>
2024-03-16 10:45:27 +0000

    Deploying to gh-pages from @ Sp3EdeR/huroutes@b2de2a9a9c71eab50aee3ea8839da5986acb6de3 🚀

--- res/huroutes.js
+++ res/huroutes.js
@@ -192,7 +192,7 @@ function androidAppUpdateToast()
 {if(!navigator.userAgent.includes('huroutes'))
 return;const ghRelease='https://api.github.com/repos/Sp3EdeR/huroutes-android/releases/latest';$.getJSON(ghRelease,data=>{try
 {const url=data.assets[0].browser_download_url;const ver=data.tag_name.slice(1);const appVer=navigator.userAgent.match(/\bhuroutes\/(\d+(?:\.\d+)*)\b/)[1];if(ver.localeCompare(appVer,undefined,{numeric:true,sensitivity:'base'})==1)
-{var androidToast=$('#toast-app-update');androidToast.find('.download-app').attr('href',url);androidToast.toast('show');}}
+{var androidToast=$('#toast-app-update');androidToast.find('a[href]').attr('href',url);androidToast.toast('show');}}
 catch
 {console.error('Invalid GitHub release JSON.');}});}}
 var sidebar={open:()=>{},close:()=>{}}
