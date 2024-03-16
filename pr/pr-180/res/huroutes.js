25bcacd29841de98b9b0fba5fd0adf0c7b9b3b00
Sp3EdeR <Sp3EdeR@users.noreply.github.com>
2024-03-16 10:47:37 +0000

    Deploy preview for PR 180 🛫

--- pr/pr-180/res/huroutes.js
+++ pr/pr-180/res/huroutes.js
@@ -186,13 +186,13 @@ function downloadString(fileName,mimeType,data,charset='utf-8')
 {var anchor=$('<a id="download" style="display:none" download="{0}"/>'.format(fileName));anchor.attr('href','data:{0};charset={1},{2}'.format(mimeType,charset,encodeURIComponent(data)));$('body').append(anchor);anchor[0].click();anchor.remove();}
 function initAdToast()
 {setTimeout(function(){androidAppToast();androidAppUpdateToast();},5000);function androidAppToast()
-{if(localStorage.shownAndroidAd||!navigator.userAgent.includes('Android')||navigator.userAgent.includes('huroutes'))
+{if(localStorage.shownAndroidAd||!navigator.userAgent.includes('Android')||navigator.userAgent.includes('huroutes')||navigator.standalone===true||window.matchMedia('(display-mode: standalone)').matches)
 return;var androidToast=$('#toast-android-app');androidToast.toast('show');androidToast.on('hide.bs.toast',()=>localStorage.shownAndroidAd=true);androidToast.find('a[href]').on('click',()=>androidToast.toast('hide'));}
 function androidAppUpdateToast()
 {if(!navigator.userAgent.includes('huroutes'))
 return;const ghRelease='https://api.github.com/repos/Sp3EdeR/huroutes-android/releases/latest';$.getJSON(ghRelease,data=>{try
 {const url=data.assets[0].browser_download_url;const ver=data.tag_name.slice(1);const appVer=navigator.userAgent.match(/\bhuroutes\/(\d+(?:\.\d+)*)\b/)[1];if(ver.localeCompare(appVer,undefined,{numeric:true,sensitivity:'base'})==1)
-{var androidToast=$('#toast-app-update');androidToast.find('.download-app').attr('href',url);androidToast.toast('show');}}
+{var androidToast=$('#toast-app-update');androidToast.find('a[href]').attr('href',url);androidToast.toast('show');}}
 catch
 {console.error('Invalid GitHub release JSON.');}});}}
 var sidebar={open:()=>{},close:()=>{}}
