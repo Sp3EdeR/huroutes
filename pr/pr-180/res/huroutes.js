a69fdf92b5bb413ff9b0f7dff5bec0990f509f60
Sp3EdeR <Sp3EdeR@users.noreply.github.com>
2024-03-16 08:48:10 +0000

    Deploy preview for PR 180 🛫

--- pr/pr-180/res/huroutes.js
+++ pr/pr-180/res/huroutes.js
@@ -215,5 +215,5 @@ return;function showToast(id)
 function pwaToast(event)
 {event.preventDefault();var toast=showToast('#toast-pwa-app');toast.find('a[href]').on('click',()=>{event.prompt();toast.toast('hide');return false;});}
 if(!localStorage.shownPwaAd)
-{window.addEventListener("beforeinstallprompt",event=>setTimeout(()=>pwaToast(event),5000));if(navigator.userAgent.match(/iP(ad|od|hone).*Safari/)&&!navigator.standalone&&!window.matchMedia('(display-mode: standalone)').matches)
+{window.addEventListener("beforeinstallprompt",event=>setTimeout(()=>pwaToast(event),5000));if(navigator.vendor&&navigator.vendor.match(/Apple/)&&navigator.userAgent&&navigator.userAgent.match(/CriOS|FxiOS/)&&!navigator.standalone&&!window.matchMedia('(display-mode: standalone)').matches)
 showToast('#toast-safari-app');}})();
